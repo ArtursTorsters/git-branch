63b7f0454925dc2684e352ac57257943d1b54d3a
Arturs Torsters <arrturs.torsters@gmail.com>
2023-11-01 06:54:41 +0200

    // branch switch detection command

--- extension.js
+++ extension.js
@@ -1,17 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+	// branch switch detection command
+	vscode.commands.registerCommand('switchBranch', () => {
+
+
+	})
+	
+
+
+
 	console.log('Congratulations, your extension "git-branch" is now active!');
 
 	// The command has been defined in the package.json file
@@ -28,7 +30,10 @@ function activate(context) {
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+return null
+
+}
 
 module.exports = {
 	activate,
