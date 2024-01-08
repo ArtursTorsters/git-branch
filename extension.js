f4d29ac88f52276689cdb657f310ecff9f56f269
Arturs Torsters <arrturs.torsters@gmail.com>
2024-01-08 19:34:01 +0200

    additional changes

--- extension.js
+++ extension.js
@@ -1,61 +1,69 @@
-const vscode = require('vscode')
-const simpleGit = require('simple-git')
+const vscode = require('vscode');
+const simpleGit = require('simple-git');
 
-//  store the OPENED files in obj
-const branchFiles = {}
+// Store the OPENED files in an object
+const branchFiles = {};
+
+console.log("branchfiles", branchFiles)
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 const activate = (context) => {
-  // dynamic path using simple git
-  const git = simpleGit(vscode.workspace.rootPath)
-  let disposable = vscode.commands.registerCommand('git-branch.helloWorld', function() {
-    let previousBranch = null
-//  detect branch
-    const detectBranchChange = () => {
-      git.branchLocal((error, summary) => {
-        const currentBranch = summary && summary.current
+  // Dynamic path using simple-git
+  const git = simpleGit(vscode.workspace.rootPath);
 
-        if (error) {
-          console.error('ERROR', error)
-        } else {
-          if (currentBranch) {
-            console.log('CURRENT BRANCH:', currentBranch)
+  // Keep track of the currently opened files
+  let openedFiles = [];
 
-            // store files for current branch
-            const openedFiles = vscode.workspace.textDocuments.map(doc => doc.fileName);
-            branchFiles[currentBranch] = openedFiles;
+  let disposable = vscode.commands.registerCommand('git-branch.helloWorld', async function() {
+    let previousBranch = null;
 
+    // Detect branch
+    const detectBranchChange = async () => {
+      const summary = await git.branchLocal();
+      const currentBranch = summary.current;
 
+      if (currentBranch) {
+        console.log('CURRENT BRANCH:', currentBranch);
 
-        //  open and show doc
-        const document = await vscode.workspace.openTextDocument(openedFiles);
-        vscode.window.showTextDocument(document);
+        // Store files for the current branch
+        branchFiles[currentBranch] = openedFiles;
 
+        // Log or perform any other actions based on your requirements
+        console.log('FILES FOR THIS BRANCH:', branchFiles[currentBranch]);
 
-            console.log("OPENED FILES" , openedFiles)
+        // Notify the user when a branch change occurs
+        if (currentBranch !== previousBranch) {
+          console.log('BRANCH CHANGED TO:', currentBranch);
+          vscode.window.showInformationMessage(`Branch changed to: ${currentBranch}`);
 
-            // Log or perform any other actions based on your requirements
-            console.log('FILES FOR THIS BRANCH:', branchFiles[currentBranch]);
+          // Close files from the previous branch
+          openedFiles.forEach(async (filePath) => {
+            const document = await vscode.workspace.openTextDocument(filePath);
+            vscode.window.showTextDocument(document);
+          });
 
-            // Notify the user when a branch change occurs
-            if (currentBranch !== previousBranch) {
-              console.log('BRANCH CHANGED TO:', currentBranch);
-              vscode.window.showInformationMessage(`Branch changed to: ${currentBranch}`);
+          // Open files for the current branch
+          branchFiles[currentBranch].forEach(async (filePath) => {
+            const document = await vscode.workspace.openTextDocument(filePath);
+            vscode.window.showTextDocument(document);
+          });
 
-              // trigger
-              previousBranch = currentBranch;
-            }
-          }
+          // Update the currently opened files
+          openedFiles = branchFiles[currentBranch];
+
+          // Trigger
+          previousBranch = currentBranch;
         }
-      });
+      }
     };
+
     detectBranchChange();
 
     // Check for branch change at regular intervals
     const interval = () => {
-      const timer = 60;
+      const timer = 20;
       setInterval(detectBranchChange, timer * 1000);
     };
     interval();
@@ -63,32 +71,27 @@ const activate = (context) => {
     vscode.window.showInformationMessage('EXTENSION ACTIVE');
   });
 
-  context.subscriptions.push(disposable);
-
   // Subscribe to the onDidOpenTextDocument event
-  // const openDisposable = vscode.workspace.onDidOpenTextDocument((document) => {
-  //   const currentBranch = git.branchLocalSync().current;
-  //   if (currentBranch) {
-  //     branchFiles[currentBranch] = branchFiles[currentBranch] || [];
-  //     branchFiles[currentBranch].push(document.fileName);
-  //   }
-  //   console.log(`FILE OPENED: ${document.fileName}`);
-  // });
+  const openDisposable = vscode.workspace.onDidOpenTextDocument((document) => {
+    openedFiles.push(document.fileName);
+    console.log(`FILE OPENED: ${document.fileName}`);
+  });
 
-  // const closeDisposable = vscode.workspace.onDidCloseTextDocument((document) => {
-  //   // Handle file close event if needed
-  //   console.log(`FILE CLOSED: ${document.fileName}`);
-  // });
+  // Subscribe to the onDidCloseTextDocument event
+  const closeDisposable = vscode.workspace.onDidCloseTextDocument((document) => {
+    // Handle file close event if needed
+    console.log(`FILE CLOSED: ${document.fileName}`);
+  });
 
-  // // Add disposables to the context subscriptions
-  // context.subscriptions.push(openDisposable, closeDisposable);
+  // Add disposables to the context subscriptions
+  context.subscriptions.push(disposable, openDisposable, closeDisposable);
 };
 
 function deactivate() {
-  return null
+  return null;
 }
 
 module.exports = {
   activate,
-  deactivate
+  deactivate,
 };
